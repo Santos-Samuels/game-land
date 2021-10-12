bab2de792cd0e0e34071c27d60af71f2e4680f39
Santos-Samuels <saamsmith15@gmail.com>
2021-10-12 19:15:07 -0300

    Página de erro e icone da pagina

---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mx-5 md:mx-10 lg:mx-20 my-8 flex flex-col items-center justify-center">
+        <div v-if="error.statusCode === 404">
+            <img class="w-1/5" src="@/static/error.gif" alt="">
+            <h1 class="text-4xl">Opps! Page not found</h1>
+            <p class="mt-2 text-xl">Sorry, the page you're looking for doesn't exist.</p>
+        </div>
+        
+        <div>
+
+        </div>
+
+        <nuxt-link class="text-center rounded py-3 w-1/4 bg-gradient-to-b from-blue-400 to-indigo-900 hover:to-indigo-700 mt-5 transition duration-700 ease-in-out" tag="a" to="/">Back to Home</nuxt-link>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'error' // you can set a custom layout for the error page
+  }
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '@/static/icon.png' },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Dosis:wght@300;400;500;600;700&display=swap'},
